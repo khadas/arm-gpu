8335e57eb1e627a9745f964315bd6b78218ab0c5
xinli.gao <xinli.gao@amlogic.com>
2023-07-05 11:00:19 +0000

    gpu: update to r44p1 for driver [2/2]
    PD#SWPL-114046
    
    Problem:
    need sync aml self patch to
    the new gpu driver version.
    
    Solution:
    sync aml self patch to r44p1
    
    Verify:
    ohm/adt4
    
    Change-Id: I92a2d6ad1b85042b35f642e23f6bca81892821e9
    Signed-off-by: xinli.gao <xinli.gao@amlogic.com>

--- bifrost/r44p1/kernel/drivers/gpu/arm/midgard/mmu/mali_kbase_mmu.c
+++ bifrost/r44p1/kernel/drivers/gpu/arm/midgard/mmu/mali_kbase_mmu.c
@@ -176,7 +176,7 @@ static void mmu_flush_pa_range(struct kbase_device *kbdev, phys_addr_t phys, siz
  * @op_param: Non-NULL pointer to struct containing information about the MMU
  *            operation to perform.
  *
- * Perform an MMU invalidate operation on a particual address space
+ * Perform an MMU invalidate operation on a particular address space
  * by issuing a UNLOCK command.
  */
 static void mmu_invalidate(struct kbase_device *kbdev, struct kbase_context *kctx, int as_nr,
@@ -2429,7 +2429,15 @@ static int mmu_insert_pages_no_flush(struct kbase_device *kbdev, struct kbase_mm
 				 * should be performed with
 				 * kbase_mmu_update_pages()
 				 */
-				WARN_ON((*target & 1UL) != 0);
+				WARN_ONCE((*target & 1UL) != 0,
+					  "vindex=0x%x, count=%d, pgd=0x%pa, "
+					  "insert_vpfn=0x%llx, start_vpfn=0x%llx, "
+					  "nr=%d, remain=%d, group_id=%d, "
+					  "is_huge_head(*phys)=%d\n",
+					  vindex, count, &pgd,
+					  insert_vpfn, start_vpfn,
+					  (u32)nr, (u32)remain, group_id,
+					  is_huge_head(*phys));
 
 				*target = kbase_mmu_create_ate(kbdev,
 					phys[i], flags, cur_level, group_id);
