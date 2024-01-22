b7c5947563a0f39e77711e3575bd395a0606cb02
Yongjie Zhu <yongjie.zhu@amlogic.com>
2024-01-22 03:03:30 +0000

    gpu: bifrost devfreq strategy update [1/1]
    PD#SWPL-154365
    
    Problem:
    gpu devfreq has defect
    
    Solution:
    1. set gpu devfreq threshold
    2. add performance gpu loading debug
    3. update devfreq thermal
    
    Verify:
    r38p1
    
    Change-Id: Ifb70a2ec69b278dd0272413fd25b916944b947f7
    Signed-off-by: Yongjie Zhu <yongjie.zhu@amlogic.com>

--- bifrost/r38p1/kernel/drivers/gpu/arm/midgard/backend/gpu/mali_kbase_pm_metrics.c
+++ bifrost/r38p1/kernel/drivers/gpu/arm/midgard/backend/gpu/mali_kbase_pm_metrics.c
@@ -75,7 +75,7 @@
  */
 enum dvfs_metric_timer_state { TIMER_OFF, TIMER_STOPPED, TIMER_ON };
 
-#ifdef CONFIG_MALI_MIDGARD_DVFS
+#if defined(CONFIG_MALI_DEVFREQ) || defined(CONFIG_MALI_MIDGARD_DVFS)
 static enum hrtimer_restart dvfs_callback(struct hrtimer *timer)
 {
 	struct kbasep_pm_metrics_state *metrics;
@@ -95,7 +95,7 @@ static enum hrtimer_restart dvfs_callback(struct hrtimer *timer)
 	hrtimer_forward_now(timer, HR_TIMER_DELAY_MSEC(metrics->kbdev->pm.dvfs_period));
 	return HRTIMER_RESTART;
 }
-#endif /* CONFIG_MALI_MIDGARD_DVFS */
+#endif
 
 int kbasep_pm_metrics_init(struct kbase_device *kbdev)
 {
@@ -154,14 +154,14 @@ int kbasep_pm_metrics_init(struct kbase_device *kbdev)
 #endif
 	spin_lock_init(&kbdev->pm.backend.metrics.lock);
 
-#ifdef CONFIG_MALI_MIDGARD_DVFS
+#if defined(CONFIG_MALI_DEVFREQ) || defined(CONFIG_MALI_MIDGARD_DVFS)
 	hrtimer_init(&kbdev->pm.backend.metrics.timer, CLOCK_MONOTONIC,
 							HRTIMER_MODE_REL);
 	kbdev->pm.backend.metrics.timer.function = dvfs_callback;
 	kbdev->pm.backend.metrics.initialized = true;
 	atomic_set(&kbdev->pm.backend.metrics.timer_state, TIMER_OFF);
 	kbase_pm_metrics_start(kbdev);
-#endif /* CONFIG_MALI_MIDGARD_DVFS */
+#endif
 
 #if MALI_USE_CSF
 	/* The sanity check on the GPU_ACTIVE performance counter
@@ -377,7 +377,7 @@ void kbase_pm_get_dvfs_metrics(struct kbase_device *kbdev,
 KBASE_EXPORT_TEST_API(kbase_pm_get_dvfs_metrics);
 #endif
 
-#ifdef CONFIG_MALI_MIDGARD_DVFS
+#if defined(CONFIG_MALI_DEVFREQ) || defined(CONFIG_MALI_MIDGARD_DVFS)
 void kbase_pm_get_dvfs_action(struct kbase_device *kbdev)
 {
 	int utilisation;
@@ -450,7 +450,7 @@ void kbase_pm_metrics_stop(struct kbase_device *kbdev)
 }
 
 
-#endif /* CONFIG_MALI_MIDGARD_DVFS */
+#endif
 
 #if !MALI_USE_CSF
 /**
